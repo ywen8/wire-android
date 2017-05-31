32bcb522634fd09b2166d0d2bbf4f74caf0f80f4
Aleksandr Bochev <red.avtovo@gmail.com>
2017-05-31 14:34:52 +0200

    added SILENT_MODE + NO_CONTACT_SHARING to CAND, EXP, QA

--- app/src/experimental/java/com.waz.zclient/PreferenceReceiver.java
+++ app/src/experimental/java/com.waz.zclient/PreferenceReceiver.java
@@ -1,6 +1,6 @@
 /**
  * Wire
- * Copyright (C) 2016 Wire Swiss GmbH
+ * Copyright (C) 2017 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+
 import com.waz.zclient.controllers.userpreferences.UserPreferencesController;
 import com.waz.zclient.core.R;
 
