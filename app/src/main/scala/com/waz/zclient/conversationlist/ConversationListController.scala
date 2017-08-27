a1491c8cf5b65cd03fd222e107825e303902302d
wireswiss <wireswiss@wire.com>
2017-08-27 18:41:10 +0100

    Add LastMessageCache in conversation list controller.
    Added cache for last messages. This way we don't need to load this information from MessagesStorage which is not suited to loading data for multipel conversations at the same time.

--- app/src/main/scala/com/waz/zclient/conversationlist/ConversationListController.scala
+++ app/src/main/scala/com/waz/zclient/conversationlist/ConversationListController.scala
@@ -4,16 +4,21 @@ import com.waz.model._
 import com.waz.service.ZMessaging
 import com.waz.threading.SerialDispatchQueue
 import com.waz.utils.events.{AggregatingSignal, EventContext, EventStream, Signal}
-import com.waz.zclient.conversationlist.ConversationListController.MembersCache
+import com.waz.zclient.conversationlist.ConversationListController.{LastMessageCache, MembersCache}
 import com.waz.zclient.views.conversationlist.ConversationAvatarView
 import com.waz.zclient.{Injectable, Injector}
+
+import scala.collection.mutable
 
 class ConversationListController(implicit inj: Injector, ec: EventContext) extends Injectable {
 
   val zms = inject[Signal[ZMessaging]]
   val membersCache = zms map { new MembersCache(_) }
+  val lastMessageCache = zms map { new LastMessageCache(_) }
 
   def members(conv: ConvId) = membersCache.flatMap(_.apply(conv))
+
+  def lastMessage(conv: ConvId) = lastMessageCache.flatMap(_.apply(conv))
 }
 
 object ConversationListController {
@@ -41,4 +46,26 @@ object ConversationListController {
 
     def apply(conv: ConvId) : Signal[Seq[UserId]] = members.map(_.getOrElse(conv, Seq.empty[UserId]))
   }
+
+
+  // Keeps last message for each conversation, this is needed because MessagesStorage is not
+  // supposed to be used for multiple conversations at the same time, as it loads an index of all conv messages.
+  // Using MessagesStorage with multiple/all conversations forces it to reload full msgs index on every conv switch.
+  class LastMessageCache(zms: ZMessaging)(implicit inj: Injector, ec: EventContext) extends Injectable {
+
+    private val cache = new mutable.HashMap[ConvId, Signal[Option[MessageData]]]
+
+    private val changeEvents = zms.messagesStorage.onChanged map { msgs => msgs.groupBy(_.convId).mapValues(_.maxBy(_.time)) }
+
+    private def updateEvents(conv: ConvId) = changeEvents.map(_.get(conv)).collect { case Some(m) => m }
+
+    private def getLastMessage(conv: ConvId) = zms.storage.db { MessageData.MessageDataDao.last(conv)(_) }
+
+    def apply(conv: ConvId): Signal[Option[MessageData]] = cache.getOrElseUpdate(conv,
+      new AggregatingSignal[MessageData, Option[MessageData]](updateEvents(conv), getLastMessage(conv), {
+        case (res @ Some(last), update) if last.time.isAfter(update.time) => res
+        case (_, update) => Some(update)
+      }))
+  }
+
 }
