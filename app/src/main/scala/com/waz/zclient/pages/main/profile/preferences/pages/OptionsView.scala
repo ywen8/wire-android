1d27a34bc525288aecf3bc78105fb96c26ba6cab
dean <deanrobertcook@gmail.com>
2017-10-26 13:28:26 +0200

    Renable CBR

--- app/src/main/scala/com/waz/zclient/pages/main/profile/preferences/pages/OptionsView.scala
+++ app/src/main/scala/com/waz/zclient/pages/main/profile/preferences/pages/OptionsView.scala
@@ -88,9 +88,7 @@ class OptionsViewImpl(context: Context, attrs: AttributeSet, style: Int) extends
   contactsSwitch.setPreference(ShareContacts)
   darkThemeSwitch.setPreference(DarkTheme)
   downloadImagesSwitch.setPreference(DownloadImagesAlways)
-  // TODO re-enable CBR after avs fix: vbrSwitch.setPreference(VBREnabled)
-  vbrSwitch.setChecked(true) //true for vbr
-  vbrSwitch.setDisabled(true)
+  vbrSwitch.setPreference(VBREnabled)
 
   vibrationSwitch.setPreference(VibrateEnabled)
   sendButtonSwitch.setPreference(SendButtonEnabled)
