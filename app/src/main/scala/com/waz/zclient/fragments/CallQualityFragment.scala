9e2cca932fa66c99d542cf5f15c9e204f806c2a8
Juliane Reschke <juliane@wire.com>
2017-12-02 18:50:08 +0100

    Added first basic skeleton to the call quality fragment class

--- app/src/main/scala/com/waz/zclient/fragments/CallQualityFragment.scala
+++ app/src/main/scala/com/waz/zclient/fragments/CallQualityFragment.scala
@@ -1,8 +1,48 @@
-package com.waz.zclient.fragments
-
 /**
-  * Created by julianereschke on 02.12.17.
+  * Wire
+  * Copyright (C) 2017 Wire Swiss GmbH
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
-class CallQualityFragment {
+
+package com.waz.zclient.fragments
+import android.app.{AlertDialog, Dialog}
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.{LayoutInflater, View}
+import android.widget.{AdapterView, ListView}
+import com.waz.zclient.controllers.SignInController
+import com.waz.zclient.fragments.CountryDialogFragment._
+import com.waz.zclient.newreg.fragments.country.{CountryCodeAdapter, CountryController}
+import com.waz.zclient.pages.BaseDialogFragment
+import com.waz.zclient.utils.ViewUtils
+import com.waz.zclient.{FragmentHelper, R}
+
+object CallQualityFragment {
 
 }
+
+class CallQualityFragment extends BaseDialogFragment[Container] with FragmentHelper with AdapterView.OnItemClickListener{
+
+  override def onCreateDialog(savedInstanceState: Bundle): Dialog = {
+
+  }
+
+  def onItemClick(adapterView: AdapterView[_], view: View, i: Int, l: Long): Unit = {
+
+  }
+}
+
+
