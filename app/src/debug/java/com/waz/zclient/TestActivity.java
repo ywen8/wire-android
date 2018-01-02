87077daf3af476edca5c42116da17057e7092cf4
dean <deanrobertcook@gmail.com>
2018-01-02 13:26:32 +0100

    Update copyright notice for 2018

--- app/src/debug/java/com/waz/zclient/TestActivity.java
+++ app/src/debug/java/com/waz/zclient/TestActivity.java
@@ -1,6 +1,6 @@
 /**
  * Wire
- * Copyright (C) 2016 Wire Swiss GmbH
+ * Copyright (C) 2018 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@ public class TestActivity extends BaseActivity {
     public void setMockControllerFactory(IControllerFactory mockControllerFactory) {
         this.mockControllerFactory = mockControllerFactory;
     }
-    
+
     @Override
     public IStoreFactory getStoreFactory() {
         return mockStoreFactory;
