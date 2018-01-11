e91af20bc3b121cc7675e01b894fda55c574cccf
dean <deanrobertcook@gmail.com>
2018-01-11 17:34:47 +0100

    Prevent NPE on animation end in ConversationListFragment

--- app/src/main/scala/com/waz/zclient/conversationlist/ConversationListFragment.scala
+++ app/src/main/scala/com/waz/zclient/conversationlist/ConversationListFragment.scala
@@ -276,7 +276,7 @@ class NormalConversationFragment extends ConversationListFragment {
       conversationListView.animate().alpha(1f).setDuration(500)
       listActionsView.animate().alpha(1f).setDuration(500)
       loadingListView.animate().alpha(0f).setDuration(500).withEndAction(new Runnable {
-        override def run() = loadingListView.setVisibility(GONE)
+        override def run() = loadingListView.view.foreach(_.setVisibility(GONE))
       })
     }
     topToolbar.setLoading(false)
