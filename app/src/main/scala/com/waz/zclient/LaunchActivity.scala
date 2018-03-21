dca1f5a69a1aa7b6a2dca7fcd1d7ee7b9514a460
dean <deanrobertcook@gmail.com>
2018-03-21 13:39:06 +0100

    Rewrite LaunchAcitivity in Scala

---
+++ app/src/main/scala/com/waz/zclient/LaunchActivity.scala
@@ -0,0 +1,75 @@
+/**
+  * Wire
+  * Copyright (C) 2018 Wire Swiss GmbH
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+package com.waz.zclient
+
+import java.util.NoSuchElementException
+
+import android.app.Activity
+import android.content.{Context, DialogInterface, Intent, SharedPreferences}
+import android.preference.PreferenceManager
+import android.support.v7.app.AlertDialog
+import com.waz.api.ClientRegistrationState.PASSWORD_MISSING
+import com.waz.api.{ClientRegistrationState, InitListener, Self}
+import com.waz.service.{BackendConfig, ZMessaging}
+import com.waz.threading.Threading
+import com.waz.zclient.appentry.AppEntryActivity
+import com.waz.zclient.utils.BackendPicker
+import com.waz.zclient.utils.Callback
+import timber.log.Timber
+
+
+class LaunchActivity extends BaseActivity {
+  override def getBaseTheme = R.style.Theme_Dark
+
+  override def onBaseActivityStart() = {
+    new BackendPicker(getApplicationContext).withBackend(this, new Callback[Void]() {
+      override def callback(aVoid: Void) = {
+        super.onBaseActivityStart()
+        ZMessaging.currentAccounts.getActiveAccount.map {
+          case Some(acc) if acc.clientRegState == PASSWORD_MISSING => startSignUp()
+          case Some(_)                                             => startMain()
+          case _                                                   => startSignUp()
+        } (Threading.Ui)
+      }
+    })
+  }
+
+  override protected def onNewIntent(intent: Intent) = {
+    super.onNewIntent(intent)
+    setIntent(intent)
+  }
+
+  // Navigation //////////////////////////////////////////////////
+  private def startMain() = {
+    startActivity(new Intent(this, classOf[MainActivity]))
+    finish()
+  }
+
+  private def startSignUp() = {
+    startActivity(new Intent(this, classOf[AppEntryActivity]))
+    finish()
+  }
+}
+
+object LaunchActivity {
+  val Tag = classOf[LaunchActivity].getName
+  val CustomBackendPref = "custom_backend_pref"
+
+}
+
+
